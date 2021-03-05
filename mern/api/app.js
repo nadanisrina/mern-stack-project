a329ae7638c825a6ef6d6c51ac05a4cdc9301c3d
Nada <nisrinaseptiana026@gmail.com>
2021-03-05 14:08:54 +0700

    init express nodemon bodyparser

--- mern/api/app.js
+++ mern/api/app.js
@@ -1,41 +1,34 @@
-const fs = require("fs");
-const http = require("http");
+const express = require("express");
+//init express
+const app = express();
 
-// example of write file system
-// const userName = "Max";
-// fs.writeFile("user-data.txt", "Name: " + userName, (err) => {
-//   if (err) {
-//     console.log("err", err);
-//     return;
-//   }
-//   console.log("WROTE FILE");
-// });
+//parse body to get user input
+app.use((req, res, next) => {
+  console.log("Function parser");
+  let body = "";
+  req.on("end", () => {
+    const userName = body.split("=")[1];
+    if (userName) {
+      req.body = { name: userName };
+    }
+    //2nd middleware
+    next();
+  });
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+});
 
-const server = http.createServer((req, res) => {
-  console.log("INCOMING REQUEST1");
-  console.log("req", req.method);
-  console.log("res", req.url);
-
-  if (req.method === "POST") {
-    let body = "";
-    req.on("end", () => {
-      //example get user input
-      const userName = body.split("=")[1];
-      console.log(body);
-      res.end(`<h1>Got the POST request ${userName}</h1>`);
-    });
-    //example get formData
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
+//2nd middleware
+app.use((req, res, next) => {
+  console.log("middleware");
+  if (req.body) {
+    return res.send("<h1>" + req.body.name + "</h1>");
   } else {
-    //example if form not submitted yet
-    res.setHeader("Content-type", "text/html");
-    res.end(
-      '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+    res.send(
+      '<form method="POST"><input type="text" name="username"><button type="submit">CREATE USERNAME</button></form>'
     );
-    //res.end("<h1>Success!</h1>");
   }
 });
 
-server.listen(5000);
+app.listen(5000);
